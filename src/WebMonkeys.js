24d84f579f0588c8547dffe003cc443363f5761a
Sultan Tarimo <sultantarimo@me.com>
2016-08-27 21:58:22 +0200

    use faster typed arrays for monkeyIndexArray

--- src/WebMonkeys.js
+++ src/WebMonkeys.js
@@ -24,7 +24,8 @@ load(this, function (exports) {
       arrays = [];
       arrayByName = {};
       shaderByTask = {};
-      monkeyIndexArray = [];
+      // monkeyIndexArray = [];
+      monkeyIndexArray = new Uint8Array(maxMonkeys);
 
       var glOpt = {antialias: false, preserveDrawingBuffer: true};
       if (typeof window === "undefined"){
